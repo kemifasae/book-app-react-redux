d1ed6d1c3e849116bbac98dc3c50b76267692f0b
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2017-11-07 17:26:55 +0100

    create book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+  return [
+    {title: 'My Book of JS' },
+    {title: 'The Quick Brown Fox'},
+    {title: 'All the Way to Calvary'},
+    {title: 'In the Beginning'},
+    {title: 'I Dreamed a Dream'}
+  ];
+}
