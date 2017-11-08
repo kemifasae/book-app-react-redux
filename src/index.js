4bf4b8861eaa0f24af4f3496ba04ff7200cb9abc
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2017-11-08 13:59:48 +0100

    add store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+// import './index.css';
+import App from './components/App';
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
+
