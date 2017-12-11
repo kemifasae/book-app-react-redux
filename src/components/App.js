b1ef371b89e9b79cd793e58b343744fe07918e88
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2017-12-11 11:12:43 +0100

    combine components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list';
-//import logo from './logo.svg';
-//import './App.css';
+import BookDetail from '../containers/book_detail';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BookList />
+        <BookDetail/>
       </div>
     );
   }
